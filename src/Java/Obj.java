a6bb8639acba9f9497ddc3de094abf7bbd7b90ba
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-02-20 10:04:14 +0530

    learn obj

---
+++ src/Java/Obj.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class Obj {
+    String name;
+    Obj(){
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        Obj ob = new Obj();
+        ob.name = "Satya";
+        System.out.println(ob.name);
+    }
+}
