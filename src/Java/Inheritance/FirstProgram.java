59f2636285119594ebe472d3d231723f0d552782
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-13 10:54:00 +0530

    first commit

---
+++ src/Java/Inheritance/FirstProgram.java
@@ -0,0 +1,81 @@
+package Java.Inheritance;
+//
+// class az {
+//    int roll = 23;
+//    String name = "ram";
+//
+//    void getValues() {
+//        System.out.println("Name:" + name + "RollNo: " + roll);
+//
+//    }
+//}
+//
+//public  class  FirstProgram extends az {
+//    public static void main(String[] args) {
+//        FirstProgram r = new FirstProgram();
+//        r.getValues();
+//       // System.out.println("hello world");
+//    }
+//
+//}
+//
+//
+//
+//class Dog {
+//    // Declaring and initializing the attributes
+//    String breed;
+//    int age;
+//    String color;
+//
+//    // methods to set breed, age, and color of the dog
+//    public void setBreed(String breed) {
+//        this.breed = breed;
+//    }
+//    public String getBreed(){
+//        return "King";
+//    }
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//    public  int getAge(){
+//        return 12;
+//    }
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//    public  String getColor(){
+//        return "green";
+//    }
+//
+//    // method to print all three values
+//    public void printDetails() {
+//        System.out.println("Dog detials:");
+//        System.out.println(getBreed());
+//        System.out.println(getAge());
+//        System.out.println(getColor());
+//    }
+//}
+//public class FirstProgram extends Dog{
+//    public static void main(String[] args) {
+//        FirstProgram r= new FirstProgram();
+//        r.printDetails();
+//    }
+//}
+
+class student{
+    int age;
+    String name;
+     int reg;
+      String address;
+
+      public  void setage(int age){
+          this.age=age;
+      }
+      public  int getAge(int age){
+          return  23;
+      }
+      public  void setname(String name){
+
+
+      }
+}
